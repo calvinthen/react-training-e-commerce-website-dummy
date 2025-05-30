ba51fcc58cbae6aab1640c989c92dd76b7e93716
Calvin Then <39335528+calvinthen@users.noreply.github.com>
2025-05-30 20:25:00 +0700

    add tailwinds and navbar skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div>
-      App
+    <div className=''>
+      <Navbar/>
     </div>
   )
 }
