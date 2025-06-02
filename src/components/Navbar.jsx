aa30be10ee03efb88f19a93cde6b8e8e271e6371
Calvin Then <39335528+calvinthen@users.noreply.github.com>
2025-06-02 18:47:49 +0700

    add navbar top and seach logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import Logo from '../assets/Shopee_logo.png'
 
 const Navbar = () => {
   return (
     <div>
-      Navbar
+       {/*Upper Navbar */} 
+      <div>
+        <div>
+          <div>
+            <a href="#">
+              <img src={Logo} alt="Logo" height={100} width={100} className='w-10 uppercase'/>
+              Shopee
+            </a>
+          </div>
+        </div>
+      </div>
+
+      {/*Lower Navbar */} 
+      <div>
+        <div className='group'>
+          <input type="text" 
+          placeholder='search'
+          className='w-[200px] sm:w-[200px] group-hover:w-[300px] transition-all duration-300 rounded-full
+          border border-gray-300 px-2 py-1 focus:outline-none focus:border-1 focus:border-primary'
+          />
+        </div>
+      </div>
+
     </div>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,19 @@ export default defineConfig({
   plugins: [react(),
     tailwindcss(),
   ],
+  theme:{
+    extend:{
+      colors:{
+        primary: "#fea928",
+        secondary: "#ed8900",
+      },
+      container:{
+        center: true,
+        padding:{
+          DEFAULT: "1rem",
+          sm: "3rem",
+        }
+      }
+    }
+  }
 })
